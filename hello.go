027bd9ef573bd47e06ae8b6a1bffca64a5de0ff3
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2017-09-05 00:51:11 -0500

    documentación

--- hello.go
+++ hello.go
@@ -9,3 +9,9 @@ func main() {
 	fmt.Print("Hola GLUD!")
 
 }
+
+
+//Nada lo unico que hace es imprimir su argumento
+func Nada(a string) {
+	fmt.Print(a)
+}
---
+++ hello_test.go
@@ -0,0 +1,15 @@
+package main
+
+
+func TestNada(t *testing.T) {
+	Nada("hola")
+
+}
+
+
+func ExampleNada() {
+	//Output:
+	// "hola"
+	Nada("hola")
+	
+}
